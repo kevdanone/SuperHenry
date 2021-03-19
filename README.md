bc2e53c3b86770762d7c9478f270a4da8a57a0db
monsieurbadia <contact@monsieurbadia.com>
2021-03-19 17:31:45 +0100

    refactoring ~main

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 ton travail si tu l'accepte nous écrire ce que tu veux
 coucou 
+
+## How to start
+
+* go to `public` folder
+* open the file `index.html` or drag and drop the file `index.html` in a browser
+* enjoy
---
+++ src/index.js
@@ -0,0 +1,8 @@
+(function (window, document) {
+  var onDOMContentLoaded = function onDOMContentLoaded() {
+    // do something
+  };
+
+  window.addEventListener('DOMContentLoaded', onDOMContentLoaded, false);
+
+}(window, window.document));
